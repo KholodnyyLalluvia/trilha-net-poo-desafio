756835984ac6766f580198829e61409291df1e8f
KholodnyyLalluvia <zerodoisb910@gmail.com>
2025-08-30 14:34:48 -0300

    completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando iPhone:");
+        Smartphone iphone = new Iphone("123456", "iPhone 14", "111111111111111", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTestando Nokia:");
+        Smartphone nokia = new Nokia("654321", "Nokia Tijolão", "222222222222222", 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+    }
+}
